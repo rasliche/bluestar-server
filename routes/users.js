1b343938466e5fad331cf93373982d6eed6067de
EricR <rasliche@gmail.com>
2018-10-09 11:38:13 -0400

    Users Routes complete

--- routes/users.js
+++ routes/users.js
@@ -1,44 +1,30 @@
+const routeDebugger = require('debug')('app:routes:users')
 const express = require('express')
 const router = express.Router()
 
-// USERS API ENDPOINTS
+const { User, validate } = require('../models/user')
 
+// USERS API ENDPOINTS
 // "/api/users"
 //     GET: finds all users
 //     POST: creates a new user
 
-router.get('/', (req,res) => {
-    User.find((error, users) => {
-        if (error) return next(error);
-        res.json(users)
-    })
+router.get('/', async (req, res) => {
+    const users = await User.find().sort({ name: 1 })
+    res.send(users)
 })
 
-router.post('/', (req,res) => {
-    var db = req.db;
-    console.log(req)
-    var firstName = req.body.firstName
-    var lastName = req.body.lastName
-    var email = req.body.email
+router.post('/', async (req, res) => {
+    routeDebugger(req.body)
+    const { error } = validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
 
-    var user = new User({
-        firstName: firstName,
-        lastName: lastName,
-        email: email
-    })
+    // Use let so customer can be reassigned
+    let user = new User({ email: req.body.email })
+    user = await user.save()
 
-    console.log(user)
-
-    user.save(function (error) {
-        if (error) {
-            console.log(error)
-        }
-        res.send({
-            success: true,
-            message: 'User saved successfully',
-            user: user
-        })
-    })
+    // send the user document back to the frontend
+    res.send(user)
 })
 
 // "/api/users/:id"
@@ -46,13 +32,38 @@ router.post('/', (req,res) => {
 //     PUT: update user by id
 //     DELETE: delete user by id
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
+    const user = await User.find({ _id: req.params.id})
+    if (!user) return res.status(404).send("A user with the provided ID was not found.")
+
+    res.send(user)
 })
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
+    // validate the request that comes from the client
+    // uses Joi validation
+    const { error } = validate(req.body) // result.error
+    // send 400 Bad Request
+    if (error) return res.status(400).send(error.details[0].message)
+
+    const user = await User.findByIdAndUpdate(req.params.id, {
+        email: req.body.email
+    },
+    {
+        new: true
+    })
+
+    if (!user) return res.status(404).send("A user with the provided ID was not found.")
+
+    // Send user back to client
+    res.send(user)
 })
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
+    const user = await User.findByIdAndDelete(req.params.id)
+    if (!user) return res.status(404).send("A user with the provided ID was not found.")
+
+    res.send(user)
 })
 
 module.exports = router
