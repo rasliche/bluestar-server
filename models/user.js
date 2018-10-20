ffb320a2d93724ccce6985ef6130f76de455854c
EricR <rasliche@gmail.com>
2018-10-19 22:27:33 -0400

    User Validation

--- models/user.js
+++ models/user.js
@@ -50,7 +50,10 @@ const User = mongoose.model('User', userSchema)
 
 function validateUser(user) {
     const schema = {
-        email: Joi.string().required()
+        firstName: Joi.string().min(1).max(50).required(),
+        lastName: Joi.string().min(1).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(8).max(255).required()
     }
     return Joi.validate(user, schema)
 }
