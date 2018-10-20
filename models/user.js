9e5222b1ee28c433b09861836b271c8f7ab66123
EricR <rasliche@gmail.com>
2018-10-19 22:27:15 -0400

    Blue Star userSchema

--- models/user.js
+++ models/user.js
@@ -5,16 +5,48 @@ const config = require('config')
 const jwt = require('jsonwebtoken')
 // Joi password complexity npm password
 
-const User = mongoose.model('User', mongoose.Schema({
-    // firstName: String,
-    // lastName: String,
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 50
+    },
     email: {
         type: String,
-        required: true
-    }//,
-    // shops: Array,
-    // role: String
-}))
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        maxlength: 1024
+    },
+    isAdmin: Boolean,
+    canModerate: {
+        type: Array
+    }
+})
+
+userSchema.methods.generateAuthToken = function() { // No arrow function
+    const token = jwt.sign({ 
+        _id: this._id, 
+        isAdmin: this.isAdmin,
+        canModerate: this.canModerate
+    }, config.get('jwtPrivateKey'))
+    return token
+}
+
+const User = mongoose.model('User', userSchema)
 
 function validateUser(user) {
     const schema = {
