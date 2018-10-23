2faf6f712263da9b10cf70153cc8ff247f475844
EricR <rasliche@gmail.com>
2018-10-22 21:57:00 -0400

    config variables

--- startup/db.js
+++ startup/db.js
@@ -5,8 +5,10 @@ const config = require('config')
 // Database Connection
 module.exports = function() {
     const db = config.get('db')
+    const dbUser = config.get('dbUser')
+    const dbPass = config.get('dbPass')
     
-    mongoose.connect(db)
+    mongoose.connect(`mongodb://${dbUser}:${dbPass}@${db}`)
         .then(() => logger.info(`Connected to ${db}...`))
         .catch(error => logger.error("Error connecting to bluestar database.", error))
 }
