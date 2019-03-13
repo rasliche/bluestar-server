9d9f7ac87054562235c86284f1c7a9553f6e5715
Eric R <rasliche@gmail.com>
2019-03-13 08:31:05 -0400

    restart

--- middleware/admin.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (req, res, next) {
-    // execute after auth middleware (sets req.user)
-    // 401 Unauthorized - Incorrect auth token
-    // 403 Forbidden - Correct token, but not allowed
-    if(!req.user.isAdmin) return res.status(403).send("User is not authorized to access this resource.")
-    next()
-}
--- middleware/error.js
+++
@@ -1,7 +0,0 @@
-const logger = require('../utilities/logger')
-
-module.exports = function(err, req, res, next) {
-    // Log the exception
-    logger.error(err.message, err)
-    res.status(500).send("Something went wrong.")
-}
