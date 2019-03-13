8a09e0b8cb8a810525d0783637a546ac1fe14f38
Eric R <rasliche@gmail.com>
2019-03-13 14:27:13 -0400

    typos

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 })
 
 app.use('/api/users/', userRoutes)
-app.use('/api/operator/', operatorRoutes)
+app.use('/api/operators/', operatorRoutes)
 app.use('/api/news/', newsRoutes)
 
 mongoose.connect('mongodb://localhost:27017/bluestar')
