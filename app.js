ed1b3f21f8eb5b1293b870d3295f77d9948b5ec0
Eric R <rasliche@gmail.com>
2019-03-13 10:02:44 -0400

    check routes, sanity check, CORS

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express')
+const userRoutes = require('./routes/users')
+const shopsRoutes = require('./routes/shops')
+const newsRoutes = require('./routes/news')
 
 const app = express()
+app.use(express.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
+
+app.use('/api/users/', userRoutes)
+app.use('/api/shops/', shopsRoutes)
+app.use('/api/news/', newsRoutes)
 
 app.listen(3000)
