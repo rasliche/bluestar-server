29366af3f77d6f91e64cab300bc9d83c83276ca1
Eric R <rasliche@gmail.com>
2019-03-13 11:15:19 -0400

    create Post model

---
+++ models/post.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String
+    },
+    content: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Post', postSchema)
