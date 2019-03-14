fe0a1b8748c91f80d1f275810ad3569b53237b24
Eric R <rasliche@gmail.com>
2019-03-14 15:24:30 -0400

    add quiz routes to app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const userRoutes = require('./routes/users')
+const quizRoutes = require('./routes/quizzes')
 const operatorRoutes = require('./routes/operators')
 const newsRoutes = require('./routes/news')
 
@@ -16,6 +17,7 @@ app.use((req, res, next) => {
 })
 
 app.use('/api/users/', userRoutes)
+app.use('/api/quizzes/', quizRoutes)
 app.use('/api/operators/', operatorRoutes)
 app.use('/api/news/', newsRoutes)
 
