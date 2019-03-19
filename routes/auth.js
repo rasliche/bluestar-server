bc774a0f89c1bfb8c4e13d2f3178e259267fde5f
Eric R <rasliche@gmail.com>
2019-03-19 15:20:17 -0400

    make sure user exists and has a valid password

--- routes/auth.js
+++ routes/auth.js
@@ -16,6 +16,12 @@ function validateLogin(user) {
     const { error } = validateLogin(req.body)
     if (error) return res.status(400).send(error.details[0].message)
 
+    let user = await User.findOne({ email: req.body.email })
+    if (!user) return res.status(400).send('Invalid email or password.')
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!validPassword) return res.status(400).send('Invalid email or password.')
+
 })
 
 module.exports = router
