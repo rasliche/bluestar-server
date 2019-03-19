428aae4264d3cf0678c4966b66d6d23b81a4718f
Eric R <rasliche@gmail.com>
2019-03-19 10:09:46 -0400

    user login route skeleton

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express')
-
 const router = express.Router()
+const { User } = require('../models/user')
+const bcrypt = require('bcrypt') // for comparing passwords
 
 router.post('/login', (req, res, next) => {
     res.send('Logged in!')
