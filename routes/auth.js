fa2bbd4a556ca9bbe651c83cb1f9c710dbf0e673
Eric R <rasliche@gmail.com>
2019-03-19 15:19:37 -0400

    validate a user login

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const Joi = require('joi')
 const { User } = require('../models/user')
 const bcrypt = require('bcrypt') // for comparing passwords
+function validateLogin(user) {
+    const schema = {
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    }
 
-router.post('/login', (req, res, next) => {
-    res.send('Logged in!')
+    return Joi.validate(user, schema)
+}
+
+
+    const { error } = validateLogin(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+
 })
 
 module.exports = router
