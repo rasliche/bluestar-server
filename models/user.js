fdf8bd4cb64b75fa6d1f3be214fcda397a10de5a
Eric R <rasliche@gmail.com>
2019-03-19 10:02:47 -0400

    Joi validation added to user model

--- models/user.js
+++ models/user.js
@@ -1,18 +1,37 @@
 const mongoose = require('mongoose')
+const Joi = require('joi')
 
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        minlength: 1,
+        maxlength: 255
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 255
     },
-    // password: {
-    //     type: String,
-    //     default: "pw"
-    // },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    },
 })
 
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(1).max(255).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    }
+
+    return Joi.validate(user, schema)
+}
+
 module.exports.User = mongoose.model('User', userSchema)
+module.exports.validateUser = validateUser
