783c1bbae6eda5bb891a9410e47a3531238930f5
Eric R <rasliche@gmail.com>
2019-03-20 13:16:40 -0400

    getAuthToken method on the user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Joi = require('joi')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -23,11 +24,16 @@ const userSchema = new mongoose.Schema({
     },
 })
 
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({ _id: this._id }, 'bluestarsecret')
+    return token
+}
+
 function validateUser(user) {
     const schema = {
         name: Joi.string().min(1).max(255).required(),
         email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(255).required()
+        password: Joi.string().min(5).max(255)
     }
 
     return Joi.validate(user, schema)
