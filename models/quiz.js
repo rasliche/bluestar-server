333ccf3249153dafc44f79cd76daea90f866fdc5
Eric R <rasliche@gmail.com>
2019-03-25 11:21:59 -0400

    quiz model, may not use but want to keep in sync with front end

--- models/quiz.js
+++ models/quiz.js
@@ -5,6 +5,18 @@ const quizSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    description: {
+        type: String,
+    },
+    questions: [
+        {
+            text: String,
+            answers: [
+                { text: String, isRight: Boolean }
+            ]
+        }
+    ],
+    lessonSlug: String
 })
 
 module.exports.Quiz = mongoose.model('Quiz', quizSchema)
