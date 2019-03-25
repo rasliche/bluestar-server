da6f9b8dc482ec2850ccf6d4edc645904b8746f8
Eric R <rasliche@gmail.com>
2019-03-25 11:20:59 -0400

    worrking on error checking and some debug output for server side console

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,15 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
+    const authHeader = req.get('Authorization')
+    if (!authHeader) return res.status(401).send('No authorization header on request.')
+
     const token = req.get('Authorization').split(' ')[1]
     const decodedToken = jwt.verify(token, 'bluestarsecret')
     // TODO: check issued at time to see if token is valid
     // expiry time?
     // send logout signal?
     if (!decodedToken) return res.status(401).send("User is not authenticated.")
+    console.log(decodedToken)
     next()
 }
