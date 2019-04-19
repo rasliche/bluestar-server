2692f4c0705a296eb2480fd9a5594bf83e06338a
Eric R <rasliche@gmail.com>
2019-04-19 18:16:43 -0400

    check for passwords again

--- routes/auth.js
+++ routes/auth.js
@@ -8,36 +8,31 @@ const _ = require('lodash')
 function validateLogin(user) {
     const schema = {
         email: Joi.string().min(5).max(255).required().email(),
-        // password: Joi.string().min(5).max(255).required()
+        password: Joi.string().min(5).max(255).required()
     }
 
     return Joi.validate(user, schema)
 }
 
-
 router.post('/login', async (req, res, next) => {
-    try {
-        const { error } = validateLogin(req.body)
-        if (error) {
-            const error = new Error("Invalid login data.")
-            error.statusCode = 400
-            throw error
-        }
-    
-        const { email } = req.body // { password }
-        let user = await User.findOne({ email: email })
-        if (!user) return res.status(400).send('Invalid email or password.')
-    
-        // const validPassword = await bcrypt.compare(password, user.password)
-        // if (!validPassword) return res.status(400).send('Invalid email or password.')
-    
-        const token = user.generateAuthToken()
-        user = _.pick(user, ['name', 'email', '_id', 'isAdmin', 'operators', 'quizScores'])
-        
-        res.send({ user, token })
-    } catch (e) {
-        console.log(e)
+    const { error } = validateLogin(req.body)
+    if (error) {
+        const error = new Error("Invalid login data.")
+        error.statusCode = 400
+        throw error
     }
+
+    const { email } = req.body // { password }
+    let user = await User.findOne({ email: email })
+    if (!user) return res.status(400).send('Invalid email or password.')
+
+    const validPassword = await bcrypt.compare(password, user.password)
+    if (!validPassword) return res.status(400).send('Invalid email or password.')
+
+    const token = user.generateAuthToken()
+    user = _.pick(user, ['name', 'email', '_id', 'isAdmin', 'operators', 'quizScores'])
+    
+    res.send({ user, token })
 })
 
 module.exports = router
