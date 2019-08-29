e52a3bb5bbd6d4a544ecd53ec4003a1544f2ed70
Eric R <rasliche@gmail.com>
2019-08-29 10:30:35 -0400

    pull db settings out into separate startup file

---
+++ startup/db.js
@@ -0,0 +1,8 @@
+const config = require('config')
+const mongoose = require('mongoose')
+
+module.exports = async function () {
+    const db = config.get('db')
+    await mongoose.connect(db)
+    console.log(`Connected to ${db}`)
+}
