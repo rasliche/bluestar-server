d35886a92caa7119acd982f1b805e72033554d63
Eric R <rasliche@gmail.com>
2019-08-29 10:34:29 -0400

    clean up file to require routes and db connection, read from config variables

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 const config = require('config')
-const express = require('express')
 require('express-async-errors')
 const helmet = require('helmet')
-const mongoose = require('mongoose')
-
-const usersRoutes = require('./routes/users')
-const quizzesRoutes = require('./routes/quizzes')
-const operatorsRoutes = require('./routes/operators')
-const lessonsRoutes = require('./routes/lessons')
-const postsRoutes = require('./routes/posts')
-const programsRoutes = require('./routes/programs')
-const authRoutes = require('./routes/auth')
-
+const express = require('express')
 const app = express()
 app.use(helmet())
-app.use(express.json())
 
 // Configuration
 console.log('Application Name: ' + config.get('name'))
@@ -29,14 +18,8 @@ app.use((req, res, next) => {
 require('./startup/routes')(app)
 require('./startup/db')()
 
-app.use((error, req, res, next) => {
-    const status = error.statusCode || 500
-    const message = error.message
-    res.status(status).json({ message: message })
-})
-
-mongoose.connect(config.get('mongo_db_uri'))
-    .then(result => {
-        app.listen(config.get('port'))
-    })
-    .catch(err => console.log(err))
+const port = config.get('port')
+const server = app.listen(port)
+console.log(`Now listening on port ${port}.`)
+    
+module.exports = server
