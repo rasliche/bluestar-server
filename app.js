f208105f0b9457e74e489a9435289834c6506b1f
Eric R <rasliche@gmail.com>
2019-08-31 19:47:17 -0400

    moving some console logs around and hiding beneath config settings

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const express = require('express')
 const app = express()
 app.use(helmet())
 
-// Configuration
-console.log('Application Name: ' + config.get('name'))
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
@@ -20,6 +17,10 @@ require('./startup/db')()
 
 const port = config.get('port')
 const server = app.listen(port)
-console.log(`Now listening on port ${port}.`)
-    
+
+if (config.util.getEnv() === 'development') {
+    console.log('Application Name: ' + config.get('name'))
+    console.log(`Now listening on port ${port}.`)
+}
+
 module.exports = server
