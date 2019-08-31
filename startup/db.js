3ead840e7c8d4c3860af821aeb839d0e012ad414
Eric R <rasliche@gmail.com>
2019-08-31 19:48:37 -0400

    no logs in testing or production

--- startup/db.js
+++ startup/db.js
@@ -4,5 +4,7 @@ const mongoose = require('mongoose')
 module.exports = async function () {
     const db = config.get('db')
     await mongoose.connect(db)
-    console.log(`Connected to ${db}`)
+    if (config.util.getEnv() === 'development') {
+        console.log(`Connected to ${db}`)
+    }
 }
