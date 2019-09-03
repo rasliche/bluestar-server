e342c8136b87d42c80325609e85544c5ff0ec3d4
Eric R <rasliche@gmail.com>
2019-09-03 09:53:23 -0400

    lesson tests passing except checking for updated dates

--- tests/integration/users.test.js
+++ tests/integration/users.test.js
@@ -156,50 +156,154 @@ describe('/api/users', () => {
     })
 
     describe('PUT /:id/records', () => {
+        let testRecord = {
+            lessonName: 'Test Lesson',
+            lessonSlug: 'test-lesson',
+            score: 50
+        }
+        let user
+
+        beforeEach(async () => {
+            user = await new User({
+                name: 'test',
+                email: 'a@a.com',
+                password: 'password'
+            }).save()
+        })
+
+        afterEach(async () => {
+            await User.deleteMany({})
+        })
+
         // Return 401 if client is not logged in
-        it('should return 401 if client is not logged in', () => {
-            expect(response.status).toBe(401)
-        })
-        
+        it('should return 401 if client is not logged in', async () => {
+            const response = await request(server)
+                .put(`/api/users/${user._id}/records`)
+                .send(testRecord)
+                expect(response.status).toBe(401)
+            })
+            
         // Return 401 if id does not match attached jwt
-        it('should return 401 if given id does not match attached jwt', () => {
-            expect(response.status).toBe(401)
-        })
-
+        it('should return 401 if given id does not match attached jwt', async () => {
+            const response = await request(server)
+                .put(`/api/users/${new mongoose.Types.ObjectId()}/records`)
+                .set('Authorization', `Bearer: ${user.generateAuthToken()}`)
+                .send(testRecord)
+                
+                expect(response.status).toBe(401)
+            })
+            
         // Return 404 if id does not exist
-        it('should return 404 if user with given id does not exist', () => {
+        it('should return 404 if user with given id does not exist', async () => {
+            user._id = new mongoose.Types.ObjectId()
+            const response = await request(server)
+                .put(`/api/users/${user._id}/records`)
+                .set('Authorization', `Bearer: ${user.generateAuthToken()}`)
+                .send(testRecord)
+            
             expect(response.status).toBe(404)
         })
 
-        // Return 404 if lesson provided does not exist
-        it('should return 404 if lesson provided does not exist', () => {
-            expect(response.status).toBe(404)
-        })
+        // Return 404 if lesson slug provided does not exist
+        // it('should return 404 if lesson provided does not exist', () => {
+
+        //     expect(response.status).toBe(404)
+        // })
 
         // Return 400 if the lesson score is not updated
-        it('should return 400 if the lesson score is not updated', () => {
-            expect(response.status).toBe(400)
-        })
+        it('should return 400 if the lesson score is not updated', async () => {
+            await request(server)
+                .put(`/api/users/${user._id}/records`)
+                .set('Authorization', `Bearer: ${user.generateAuthToken()}`)
+                .send(testRecord)
 
+            const response1 = await request(server) // Test if a lower score updates
+                .put(`/api/users/${user._id}/records`)
+                .set('Authorization', `Bearer: ${user.generateAuthToken()}`)
+                .send({
+                    lessonName: 'Test Lesson',
+                    lessonSlug: 'test-lesson',
+                    score: 30 // lower than testRecord.score (50)
+                })
+
+            const response2 = await request(server) // Test that score isn't updated
+                .put(`/api/users/${user._id}/records`)
+                .set('Authorization', `Bearer: ${user.generateAuthToken()}`)
+                .send(testRecord)
+
+            expect(response1.status).toBe(400)
+            expect(response2.status).toBe(400)
+        })
+        
         // Should return 200 if the record is updated successfully
-        it('should return 200 if the record is updated', () => {
+        it('should return 200 if the record is updated', async () => {
+            await request(server)
+                .put(`/api/users/${user._id}/records`)
+                .set('Authorization', `Bearer: ${user.generateAuthToken()}`)
+                .send(testRecord)
+
+            const response = await request(server) // Test if a higher score updates
+                .put(`/api/users/${user._id}/records`)
+                .set('Authorization', `Bearer: ${user.generateAuthToken()}`)
+                .send({
+                    lessonName: 'Test Lesson',
+                    lessonSlug: 'test-lesson',
+                    score: 60 // higher than testRecord.score (50)
+                })
+
             expect(response.status).toBe(200)
         })
-
+        
         // Should return 201 if the record is new for the user
-        it('should return 201 if the record is new for the user', () => {
+        it('should return 201 if the record is new for the user', async () => {
+            const response = await request(server)
+                .put(`/api/users/${user._id}/records`)
+                .set('Authorization', `Bearer: ${user.generateAuthToken()}`)
+                .send(testRecord)
+
+            const newRecordOnUser = response.body.lessonScores.find(r => r.lessonSlug === testRecord.lessonSlug)
             expect(response.status).toBe(201)
+            expect(response.body.lessonScores.length).toBe(1)
+            expect(newRecordOnUser).toHaveProperty('lessonSlug', testRecord.lessonSlug)
+            expect(newRecordOnUser).toHaveProperty('score', testRecord.score)
         })
 
         // Should return the user record
-        it('should return the user record', () => {
-            expect(response.body).toHaveProperty('user')
+        it('should return the user record', async () => {
+            const response = await request(server)
+                .put(`/api/users/${user._id}/records`)
+                .set('Authorization', `Bearer: ${user.generateAuthToken()}`)
+                .send(testRecord)
+            expect(response.body).toHaveProperty('name', user.name)
+            expect(response.body).toHaveProperty('email', user.email)
+            expect(response.body).not.toHaveProperty('password')
         })
 
         // Should update the date of record when updating score
-        it('should update the date of record when updating score', () => {
-            expect(true).toBe(false)
-        })
+        it('should update the date of record when updating score', async () => {
+            await request(server)
+                .put(`/api/users/${user._id}/records`)
+                .set('Authorization', `Bearer: ${user.generateAuthToken()}`)
+                .send(testRecord)
+        
+            const response = await request(server)
+                .put(`/api/users/${user._id}/records`)
+                .set('Authorization', `Bearer: ${user.generateAuthToken()}`)
+                .send({
+                    lessonName: 'Test Lesson',
+                    lessonSlug: 'test-lesson',
+                    score: 60
+                })
+        
+            const newRecordOnUser = response.body.lessonScores.find(r => r.lessonSlug === testRecord.lessonSlug)
+            const lessonScores = await User.findById(user._id).select('lessonScores')
+            const rawRecord = lessonScores.find(r => r.lessonSlug === testRecord.lessonSlug)
 
+            expect(response.status).toBe(200)
+            expect(response.body.lessonScores.length).toBe(1)
+            expect(newRecordOnUser).toHaveProperty('lessonSlug', testRecord.lessonSlug)
+            expect(newRecordOnUser).toHaveProperty('score', testRecord.score)
+            expect(newRecordOnUser.date).toBe(rawRecord.date)
+        })
     })
 })
