706c7813de76b8295d3f3a97c69ecaea520d8682
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-09-23 20:02:56 -0700

    Lint middleware

--- middleware/attachUser.js
+++ middleware/attachUser.js
@@ -1,10 +1,10 @@
-const { User } = require('../models/user')
+const { User } = require('../models/user');
 
-// This middleware should extract the token 
+// This middleware should extract the token
 module.exports = async (req, res, next) => {
-    const user = await User.findById(req.token._id)
-    if (!user) { return res.status(404).send("No user found for given ID") }
+  const user = await User.findById(req.token._id);
+  if (!user) { return res.status(404).send('No user found for given ID'); }
 
-    req.user = user
-    next()
-}
+  req.user = user;
+  next();
+};
--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,15 +1,15 @@
-const jwt = require('jsonwebtoken')
-const config = require('config')
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 module.exports = (req, res, next) => {
-    const authHeader = req.header('Authorization')
-    if (!authHeader) { return res.status(401).send("No authorization header on request.") }
+  const authHeader = req.header('Authorization');
+  if (!authHeader) { return res.status(401).send('No authorization header on request.'); }
 
-    const token = authHeader.split(' ')[1] // 'Bearer: jwtTokenString'
-    if (!token) { return res.status(401).send('No authorization token present.')}
-    jwt.verify(token, config.get('jwtPrivateKey'), (error, decodedToken) => {
-        if (error) { return res.status(401).send('User authorization token is not able to be verified.')}
-        req.token = decodedToken
-    })
-    next()
-}
+  const token = authHeader.split(' ')[1]; // 'Bearer: jwtTokenString'
+  if (!token) { return res.status(401).send('No authorization token present.'); }
+  jwt.verify(token, config.get('jwtPrivateKey'), (error, decodedToken) => {
+    if (error) { return res.status(401).send('User authorization token is not able to be verified.'); }
+    req.token = decodedToken;
+  });
+  next();
+};
