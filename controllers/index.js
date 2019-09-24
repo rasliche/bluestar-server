1f17271bcbcbb99c721ba8d1e821f381d41c2189
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-09-23 19:32:57 -0700

    Index controllers

---
+++ controllers/index.js
@@ -0,0 +1,17 @@
+const authController = require('./auth');
+const lessonController = require('./lesson');
+const operatorController = require('./operator');
+const postController = require('./post');
+const programController = require('./program');
+const quizController = require('./quiz');
+const userController = require('./user');
+
+module.exports = {
+  authController,
+  lessonController,
+  operatorController,
+  postController,
+  programController,
+  quizController,
+  userController,
+};
