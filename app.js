babc136e9545cf02eaedf05584e2d33d284d06f6
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-09-23 21:40:12 -0700

    Refactor app server

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 require('express-async-errors');
 const config = require('config');
 const express = require('express');
-const compression = require('compression');
 const morgan = require('morgan');
+const compression = require('compression');
 const helmet = require('helmet');
-
 const cors = require('cors');
 const routes = require('./routes');
 const { logError, handleError } = require('./middleware');
 
 const port = config.get('port');
-
 const app = express();
 
 // Log requests
@@ -23,11 +21,13 @@ app.use(express.urlencoded({ extended: true }));
 // gzip compression
 app.use(compression());
 
-app.use(cors());
-
+// Set HTTP headers for server security
 app.use(helmet());
 
+// Enable all Cross-Origin Resource Sharing
+app.use(cors());
 
+// API routes
 app.use('/', routes);
 app.use(logError);
 app.use(handleError);
