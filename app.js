c25a68d03f9a1075546f7c03d2cec2f58fd40ed6
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-09-23 18:59:03 -0700

    Streamline server initialization

--- app.js
+++ app.js
@@ -1,18 +1,51 @@
-const config = require('config')
-require('express-async-errors')
-const express = require('express')
-const app = express()
+require('express-async-errors');
+const config = require('config');
+const helmet = require('helmet');
+const express = require('express');
+const mongoose = require('mongoose');
+const usersRoutes = require('./routes/users');
+const quizzesRoutes = require('./routes/quizzes');
+const operatorsRoutes = require('./routes/operators');
+const lessonsRoutes = require('./routes/lessons');
+const postsRoutes = require('./routes/posts');
+const programsRoutes = require('./routes/programs');
+const authRoutes = require('./routes/auth');
 
-require('./startup/config')(app)
-require('./startup/routes')(app)
-require('./startup/db')()
+const app = express();
 
-const port = config.get('port')
-const server = app.listen(port)
+app.use(helmet());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use(express.json());
+
+app.use('/api/users/', usersRoutes);
+app.use('/api/quizzes/', quizzesRoutes);
+app.use('/api/operators/', operatorsRoutes);
+app.use('/api/lessons/', lessonsRoutes);
+app.use('/api/posts/', postsRoutes);
+app.use('/api/programs/', programsRoutes);
+app.use('/api/auth/', authRoutes);
+app.use((error, req, res) => {
+  const status = error.statusCode || 500;
+  const message = error.message || 'Server error.';
+  res.status(status).send(message);
+});
+
+const db = config.get('db');
+mongoose.connect(db);
 if (config.util.getEnv() === 'development') {
-    console.log('Application Name: ' + config.get('name'))
-    console.log(`Now listening on port ${port}.`)
+  console.log(`Connected to ${db}`);
 }
 
-module.exports = server
+const port = config.get('port');
+
+app.listen(port, () => {
+  console.log(`Application Name: ${config.get('name')}`);
+  console.log(`Now listening on port ${port}.`);
+});
