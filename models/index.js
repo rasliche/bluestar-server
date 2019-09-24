527b4678ddfc1f0d10290d0b249d082a4e37574c
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-09-23 18:30:36 -0700

    Index models

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const Lesson = require('./lesson');
+const Operator = require('./operator');
+const Post = require('./post');
+const Program = require('./program');
+const Quiz = require('./quiz');
+const User = require('./user');
+
+
+module.exports = {
+  Lesson, Operator, Post, Program, Quiz, User,
+};
