8e906f908dfd8437e3ed23914cfc03b83a3a32ba
Glen Paul Florendo <9373317+glenflorendo@users.noreply.github.com>
2019-09-23 17:57:30 -0700

    Configure linter

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    indent: ["error", 2],
+    'no-console': 'off',
+    'no-underscore-dangle': 'off',
+    'func-names': 'off'
+  },
+};
