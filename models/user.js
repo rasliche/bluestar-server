bc9f9d8adc72c1b163f9c37afdf01d979eef0757
Eric R <rasliche@gmail.com>
2019-09-28 21:47:19 -0400

    Fields to increment login counts and last login date

--- models/user.js
+++ models/user.js
@@ -56,6 +56,12 @@ const userSchema = new mongoose.Schema({
     isAdmin: {
         type: Boolean,
         default: false,
+    },
+    lastLogin: {
+        type: Date,
+    },
+    loginCount: {
+        type: Number,
     }
 })
 
