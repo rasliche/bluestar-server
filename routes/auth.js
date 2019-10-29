8e971302d2044ad281b4b8b4916568da23a8650a
Eric R <rasliche@gmail.com>
2019-10-29 12:24:31 -0400

    user is immutable

--- routes/auth.js
+++ routes/auth.js
@@ -18,7 +18,7 @@ router.post('/login', async (req, res, next) => {
     const { error } = validateLogin(req.body)
     if (error) { return res.status(400).send('Invalid email or password') }
 
-    let user = await User.findOne({ email: req.body.email })
+    const user = await User.findOne({ email: req.body.email })
     if (!user) { return res.status(400).send('Invalid email or password.') }
 
     const validPassword = await bcrypt.compare(req.body.password, user.password)
