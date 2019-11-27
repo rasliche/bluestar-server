e4ca8d1a8f6bb14b150d2e08993396893448597d
Eric R <rasliche@gmail.com>
2019-11-27 10:11:38 -0500

    address deprecation warnings for mongoose

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose')
 
 module.exports = async function () {
     const db = config.get('db')
-    await mongoose.connect(db, { useNewUrlParser: true, })
+    await mongoose.connect(db, { 
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+
+    })
     if (config.util.getEnv() === 'development') {
         console.log(`Connected to ${db}`)
     }
