969e1182973ec4d253c399e7097e56fa94a58248
Eric R <rasliche@gmail.com>
2020-01-02 17:03:25 -0500

    Spacing fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,29 +3,27 @@ const { User } = require('../models/user')
 const bcrypt = require('bcrypt') // for comparing passwords
 
 function validateLogin(user) {
-    const schema = {
-        email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(255).required()
-    }
+  const schema = {
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required()
+  }
 
-    return Joi.validate(user, schema)
+  return Joi.validate(user, schema)
 }
 
 exports.create = async (req, res, next) => {
-    const { error } = validateLogin(req.body)
-    if (error) { return res.status(400).send('Invalid email or password') }
+  const { error } = validateLogin(req.body)
+  if (error) { return res.status(400).send('Invalid email or password') }
 
-    const user = await User.findOne({ email: req.body.email })
-    if (!user) { return res.status(400).send('Invalid email or password.') }
+  const user = await User.findOne({ email: req.body.email })
+  if (!user) { return res.status(400).send('Invalid email or password.') }
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
-    if (!validPassword) { return res.status(400).send('Invalid email or password') }
+  const validPassword = await bcrypt.compare(req.body.password, user.password)
+  if (!validPassword) { return res.status(400).send('Invalid email or password') }
 
-    // Increase loginCount so we can keep track of active accounts
-    // await User.findByIdAndUpdate(user._id, { $inc: user.loginCount, 'user.lastLogin': new Date().now })
-    
-    const token = user.generateAuthToken()
-    const { password, ...userWithoutPassword } = user.toObject()
-    
-    res.send({ ...userWithoutPassword, token })
+  const token = user.generateAuthToken()
+  const { password, ...userWithoutPassword } = user.toObject()
+
+  // TODO: Refactor to send a single user object
+  res.send({ ...userWithoutPassword, token })
 }
