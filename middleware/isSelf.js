2c2a9553a068b80067bca495f98aecf2d474f5a2
Eric R <rasliche@gmail.com>
2020-01-02 17:03:35 -0500

    placeholder middleware

---
+++ middleware/isSelf.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+module.exports = (req, res, next) => {
+    const authHeader = req.header('Authorization')
+    if (!authHeader) { return res.status(401).send("No authorization header on request.") }
+
+    const token = authHeader.split(' ')[1] // 'Bearer: jwtTokenString'
+    if (!token) { return res.status(401).send('No authorization token present.')}
+    jwt.verify(token, config.get('jwtPrivateKey'), (error, decodedToken) => {
+        if (error) { return res.status(401).send('User authorization token is not able to be verified.')}
+        if (config.util.getEnv('NODE_ENV') === 'development') { console.log(decodedToken) }
+        req.user = decodedToken
+    })
+    next()
+}
