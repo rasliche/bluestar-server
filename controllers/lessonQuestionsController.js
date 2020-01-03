24cb8e2a7ee911464f0f87c935d92c8f3c6e5f79
Eric R <rasliche@gmail.com>
2020-01-03 15:39:25 -0500

    create and destroy questions for a lesson

--- controllers/lessonQuestionsController.js
+++ controllers/lessonQuestionsController.js
@@ -1,11 +1,24 @@
-exports.index = (req, res, next) => {
-  console.log(req.params)
-  res.send(req.params)
+const { Lesson } = require('../models/lesson')
+const { Question, validateQuestion } = require('../models/question')
+
+exports.index = async (req, res, next) => {
+  const { questions } = await Lesson.findById(req.params.lessonId).populate('questions')
+  res.send(questions)
 }
 
-exports.create = (req, res, next) => {
-  
-  res.send('create')
+exports.create = async (req, res, next) => {
+  try {
+    const lesson = await Lesson.findById(req.params.lessonId)
+    if (!lesson) return res.status(404).send("Lesson with given ID not found.")
+    const { error } = validateQuestion(req.body)
+    if (error) return res.status(400).send('Invalid question provided.')
+    const question = await new Question({ ...req.body }).save()
+    lesson.questions.push(question._id)
+    await lesson.save()
+    res.send(question)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 exports.read = (req, res, next) => {
@@ -18,7 +31,16 @@ exports.update = (req, res, next) => {
   res.send('update')
 }
 
-exports.destroy = (req, res, next) => {
-  
-  res.send('destroy')
+exports.destroy = async (req, res, next) => {
+  try {
+    const lesson = await Lesson.findById(req.params.lessonId)
+    if (!lesson) return res.status(404).send("Lesson with given ID not found.")
+    const question = await Question.findByIdAndDelete(req.params.questionId)
+    if(!question) return res.status(404).send("Question with the given ID not found.")
+    lesson.questions.pull(question)
+    await lesson.save()
+    res.send(question)
+  } catch (error) {
+    console.log(error)
+  }
 }
