634e88567f23626787e31ea1f00c27885f8c5002
Eric R <rasliche@gmail.com>
2020-01-03 13:13:48 -0500

    Figured out how to persist req.params

--- startup/routes.js
+++ startup/routes.js
@@ -1,13 +1,15 @@
 module.exports = function(app) {
+    app.use('/api/auth/', require('../routes/authRoutes'))
     app.use('/api/users/', require('../routes/usersRoutes'))
-    app.use('/api/user/', require('../routes/userScoresRoutes'))
+    app.use('/api/user/:userId/scores/', require('../routes/userScoresRoutes'))
     app.use('/api/questions/', require('../routes/questionsRoutes'))
     app.use('/api/operators/', require('../routes/operatorsRoutes'))
     app.use('/api/lessons/', require('../routes/lessonsRoutes'))
-    app.use('/api/lesson/', require('../routes/lessonQuestionsRoutes')) // :lessonId/questions/
+    app.use('/api/lesson/:lessonId/questions/', require('../routes/lessonQuestionsRoutes'))
+    app.use('/api/lesson/:lessonId/programs/', require('../routes/lessonProgramsRoutes'))
+    app.use('/api/published-lessons/', require('../routes/publishedLessonsRoutes'))
     app.use('/api/posts/', require('../routes/postsRoutes'))
     app.use('/api/programs/', require('../routes/programsRoutes'))
-    app.use('/api/auth/', require('../routes/authRoutes'))
 
     app.use((error, req, res, next) => {
         const status = error.statusCode || 500
