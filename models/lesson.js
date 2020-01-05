86cfdd920813fa05f0f3e587aa4091cfe9fb401f
Eric R <rasliche@gmail.com>
2020-01-05 16:51:22 -0500

    deeper coverPhoto field for a lesson

--- models/lesson.js
+++ models/lesson.js
@@ -10,6 +10,16 @@ const lessonSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    coverPhoto: {
+        location: {
+            type:String,
+            default: 'https://res.cloudinary.com/duzmgsio4/image/upload/v1554164246/fknms-bluestar/about-fknms/aerial-reef.jpg'
+        },
+        altDescription: {
+            type: String,
+            default: "Aerial view of a coral reef in the Florida Keys."
+        },
+    },
     content: {
         type: mongoose.Schema.Types.Mixed
     },
@@ -31,6 +41,7 @@ function validateLesson(lesson) {
     const schema = {
         title: Joi.string().required(),
         description: Joi.string().required(),
+        coverPhoto: Joi.string(),
         content: Joi.object(),
         published: Joi.boolean(),
         programs: Joi.any(),
