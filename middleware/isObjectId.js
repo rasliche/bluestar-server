30e33a763b5dcfa8da66b51dace3c3cfc4c3297e
Eric R <rasliche@gmail.com>
2020-01-17 17:25:26 -0500

    Broken middleware

--- middleware/isObjectId.js
+++ middleware/isObjectId.js
@@ -1,11 +1,11 @@
 const { param, validationResult } = require('express-validator')
 
-module.exports = async (req, res, next) => {
+module.exports = (req, res, next) => {
     console.log(req.params)
     param().isMongoId()
-    // console.log(validationResult(req))
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
+    validationResult(req).throw()
+    // const result = validationResult(req)
+    if (!result.isEmpty()) {
         const error = new Error("Invalid MongoID received in URL.")
         error.statusCode = 422
         next(error)
