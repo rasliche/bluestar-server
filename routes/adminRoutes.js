149a3ed5a8c5b1884300d3ce26921c3ec0bb406a
Eric R <rasliche@gmail.com>
2020-02-01 16:48:16 -0500

    Validate admin user creation

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,26 @@
 const router = require('express').Router()
 const adminController = require('../controllers/adminController')
+const { body } = require('express-validator')
 
 router.get('/', adminController.index)
 
-router.post('/', adminController.create)
+router.post('/', 
+    [
+        body('name')
+            .trim()
+            .isLength({ min: 1, max: 255 }),
+        body('email')
+            .trim()
+            .isEmail()
+            .normalizeEmail()
+            .isLength({ min: 5, max:255 }),
+        body('password')
+            .trim()
+            .isLength({ min: 6 }),
+        body('adminPass')
+            .exists()
+    ], 
+    adminController.create)
 
 router.get('/:id', adminController.read)
 
