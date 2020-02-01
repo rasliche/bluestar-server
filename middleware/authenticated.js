98fe8e75c8ab7dd3cc01912439660fc7bea71690
Eric R <rasliche@gmail.com>
2020-02-01 16:47:30 -0500

    Look up User from token and attach to req going forward

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 const config = require('config')
+const { User } = require('../models/user')
 
 module.exports = (req, res, next) => {
     const authHeader = req.header('Authorization')
@@ -8,10 +9,13 @@ module.exports = (req, res, next) => {
     const token = authHeader.split(' ')[1] // 'Bearer: jwtTokenString'
     if (!token) { return res.status(401).send('No authorization token present.')}
     
-    jwt.verify(token, config.get('jwtPrivateKey'), (error, decodedToken) => {
+    jwt.verify(token, config.get('jwtPrivateKey'), async (error, decodedToken) => {
         if (error) { return res.status(401).send('User authorization token is not able to be verified.')}
         if (config.util.getEnv('NODE_ENV') === 'development') { console.log(decodedToken) }
-        req.user = decodedToken
+        
+        const user = await User.findById(decodedToken)
+        if (!user) { return res.status(401).send('User not found for provided token ID.') }
+        req.user = user
     })
     next()
 }
