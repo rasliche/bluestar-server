0d825f09462a98bdfb1074e4f0b76d097dc6a47d
Eric R <rasliche@gmail.com>
2020-02-01 16:50:20 -0500

    Refactor /me route to use req.user attached after token

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router()
 const userController = require('../controllers/userController')
-
 const auth = require('../middleware/authenticated')
 
 router.get('/', userController.index)
@@ -10,20 +9,7 @@ router.post('/', userController.create)
 router.get('/:id', userController.read)
 
 router.get('/me', [auth], async (req, res, next) => {
-    const { token } = req
-    // if (!token) {
-    //     const error = new Error('No authentication token provided.')
-    //     error.statusCode = 401
-    //     throw error
-    // }
-    const user = await User.findById(token._id)
-    if (!user) { res.status(404).send('No user found with current jwt.') }
-    //     const error = new Error('No user found with current jwt.')
-    //     error.statusCode = 404
-    //     throw error
-    // }
-    // token = user.generateAuthToken()
-    const { password, ...userWithoutPassword } = user.toObject()
+    const { password, ...userWithoutPassword } = req.user.toObject()
     res.send(userWithoutPassword)
 })
 
