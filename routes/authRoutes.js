45fc2d09be114cc6e000a57f6db41471ee89fcdd
Eric R <rasliche@gmail.com>
2020-02-01 16:47:49 -0500

    Validate login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,18 @@
 const router = require('express').Router()
-const { body } = require('express-validator')
+const { body, sanitizeBody } = require('express-validator')
 
 const authController = require('../controllers/authController')
 
 router.post('/login', 
     [
-        body('email').isEmail().notEmpty(),
-        body('password').isLength({ min: 6 }).notEmpty()
+        body('email')
+            .trim()
+            .isEmail()
+            .normalizeEmail()
+            .isLength({ min: 5, max:255 }),
+        body('password')
+            .trim()
+            .isLength({ min: 6 })
     ], 
     authController.create)
 
