45739f13c218cc4b7857d6b5242a61bace541aba
Eric R <rasliche@gmail.com>
2020-02-02 12:15:38 -0500

    Modify config to use more db environment variables

--- startup/db.js
+++ startup/db.js
@@ -2,14 +2,18 @@ const config = require('config')
 const mongoose = require('mongoose')
 
 module.exports = async function () {
-    const db = config.get('db')
-    await mongoose.connect(db, { 
+    const db = {}
+    db.mongo_hostname = config.get('db.mongo_hostname')
+    db.mongo_port = config.get('db.mongo_port')
+    db.mongo_db = config.get('db.mongo_db')
+    
+    await mongoose.connect(`${db.mongo_hostname}:${db.mongo_port}/${db.mongo_db}`, { 
         useNewUrlParser: true,
         useCreateIndex: true,
         useUnifiedTopology: true,
 
     })
     if (config.util.getEnv('NODE_ENV') === 'development') {
-        console.log(`Connected to ${db}`)
+        console.log(`Connected to the ${db.mongo_db} database on ${db.mongo_hostname}:${db.mongo_port}`)
     }
 }
