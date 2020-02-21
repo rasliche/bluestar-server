fe772a01b36edebcd95c148e83d2b621239566b0
Eric R <rasliche@gmail.com>
2020-02-21 10:33:03 -0500

    Add console.log to note an admin request was made

--- middleware/admin.js
+++ middleware/admin.js
@@ -3,5 +3,6 @@
 
 module.exports = (req, res, next) => {
     if (!req.user.isAdmin) return res.status(403).send("User is forbidden from accessing this resource.")
+    console.log("Admin request.")
     next()
 }
