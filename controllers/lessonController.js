4ea5c45e709fdbbc69ba1df05a55f2f52c7367a7
Eric R <rasliche@gmail.com>
2020-02-21 10:54:05 -0500

    Remove old doo doo validation

--- controllers/lessonController.js
+++ controllers/lessonController.js
@@ -1,18 +1,11 @@
-const { Lesson, validateLesson } = require('../models/lesson')
+const { Lesson } = require('../models/lesson')
 
 exports.index = async (req, res, next) => {
-  try {
     const lessons = await Lesson.find().populate('programs')
     res.send(lessons)
-  } catch (error) {
-    next(error)
-  }
 }
 
 exports.create = async (req, res, next) => {
-  const { error } = validateLesson(req.body)
-  if (error) return res.status(400).send(error)
-
   const lesson = new Lesson({
     title: req.body.title,
     description: req.body.description,
@@ -26,30 +19,20 @@ exports.create = async (req, res, next) => {
 }
 
 exports.read = async (req, res, next) => {
-  const query = Lesson.findById(req.params.id)
-  if (req.query.questions === 'true') query.populate('questions')
-  if (req.query.programs === 'true') query.populate('programs')
-  try {
-    const lesson = await query.exec()
+    const lesson = await Lesson.findById(req.params.id)
+    if (!lesson) return res.status(404).send("Lesson with given ID not found.")
+
     res.send(lesson)
-  } catch (error) {
-    next(error)
-  }
 }
 
 exports.update = async (req, res, next) => {
-  const { error } = validateLesson(req.body)
-  if (error) return res.status(400).send(error)
-    
   const lesson = await Lesson.findByIdAndUpdate(req.params.id, {
-    // slug: slug(req.body.title),
     title: req.body.title,
     description: req.body.description,
     programs: req.body.programs,
-    content: req.body.content,
   }, { new: true })
-  if (!lesson) return res.status(404).send("Lesson with given ID not found.")
 
+  if (!lesson) return res.status(404).send("Lesson with given ID not found.")
   res.send(lesson)
 }
 
