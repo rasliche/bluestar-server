fd61d903fc9e0080d75a32a58dbc3cd8d3ea8ce7
Eric R <rasliche@gmail.com>
2020-04-24 20:42:10 -0400

    Users can now join a shop ONCE

--- controllers/userOperatorsController.js
+++ controllers/userOperatorsController.js
@@ -2,10 +2,10 @@ const { User } = require('../models/user')
 const { Operator } = require('../models/operator')
 
 exports.index = async (req, res, next) => {
-  const userOperators = await User.findById(req.params.userId)
+  const { operators } = await User.findById(req.params.userId)
     .select('operators')
     .populate({ path: 'operators.operator', select: 'name' })
-  res.send(userOperators)
+  res.send(operators)
 }
 
 exports.create = async (req, res, next) => {
@@ -14,23 +14,23 @@ exports.create = async (req, res, next) => {
   const operator = await Operator.findById(req.body.shopId)
   if (!operator) return res.status(404).send('Operator not found for the given ID.')
 
-  const alreadyAssociated = user.operators.find(r => r.operator === req.body.shopId)
-  console.log(`Already Assciated? ${alreadyAssociated}`)
+  const alreadyAssociated = user.operators.find(r => r.operator.toString() === req.body.shopId)
   if (alreadyAssociated) {
-    console.log(alreadyAssociated)
-    return res.status(400).send('User is already associated with the give Operator.')
+    return res.status(400).send('User is already associated with the given operator.')
   }
 
   if (operator.password !== req.body.shopPassword) {
-    return res.status(400).send('Provided password is invalid for the given shop.')
+    return res.status(400).send('Provided password is invalid for the given operator.')
   }
 
-  user.operators.push({
-    operator: operator._id
-  })
+  const newOperator = {
+    operator: operator._id,
+    canManage: user.isAdmin
+  }
+  user.operators.push(newOperator)
   await user.save()
-  
-  res.send(operator)
+
+  res.send({ operator, canManage: user.isAdmin })
 }
 
 exports.read = (req, res, next) => {
